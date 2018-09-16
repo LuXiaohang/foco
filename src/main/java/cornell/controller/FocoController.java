9b247f8c1c4e259405be835d84ab295d104fb749
LuXiaohang <1300016603@pku.edu.cn>
2018-09-15 21:26:42 -0400

    login

--- src/main/java/cornell/controller/FocoController.java
+++ src/main/java/cornell/controller/FocoController.java
@@ -1,22 +1,27 @@
 package cornell.controller;
 
+import cornell.dao.ProfileForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class FocoController{
-    @RequestMapping("/hello")
-    public String hello(@RequestParam("name")String webName, Model model){
-        model.addAttribute("message","Hello, This is "+webName);
-        return "resultPage";
-    }
+    @RequestMapping("/login.html")
+    public String login(ProfileForm profileForm){ return "login"; }
 
     @RequestMapping("/index.html")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(value = "/profile",method = RequestMethod.POST)
+    public String displayProfile(ProfileForm profileForm,Model model){
+        model.addAttribute("message",profileForm.getUsername()+profileForm.getPassword());
+        return "resultPage";
     }
 }
